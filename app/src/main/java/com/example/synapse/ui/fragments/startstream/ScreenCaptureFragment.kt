6b737afb41b3b7522ad334cdaf83e34747611baa
Abhi <abhishekvelekar79200@gmail.com>
2024-07-27 12:37:12 +0530

    Screen Capture fragment layout added

---
+++ app/src/main/java/com/example/synapse/ui/fragments/startstream/ScreenCaptureFragment.kt
@@ -0,0 +1,8 @@
+package com.example.synapse.ui.fragments.startstream
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ScreenCaptureFragment : Fragment() {
+}
